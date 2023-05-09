5bbcd2ec9d97ba418fbed63e53d938e94806113a
Leandro-Aballone <l.aballone@gmail.com>
2023-05-09 17:56:08 -0300

    Ejercicio 1

---
+++ README.md
@@ -0,0 +1,1 @@
+# CaC4.0-Practica-JS
---
+++ index.js
@@ -0,0 +1,15 @@
+
+//Ejercicio 1:
+/*1. **Declaración de variables**
+    - Declara una variable llamada **`nombre`** y asígnale un valor de tipo string.
+    - Declara una variable llamada **`edad`** y asígnale un valor de tipo number.
+    - Declara una variable llamada **`esEstudiante`** y asígnale un valor de tipo boolean.
+*/
+
+console.log("Ejercicio 1: \n");
+let nombre = "Nombre1";
+let edad = 23;
+let esEstudiante = true;
+
+console.log(nombre, edad, esEstudiante);
+
