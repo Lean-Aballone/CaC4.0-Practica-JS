433c6ed84a3815f2ecca1000d5e837483bc61947
Leandro-Aballone <l.aballone@gmail.com>
2023-05-09 18:10:45 -0300

    Ejercicio 3

--- index.js
+++ index.js
@@ -21,25 +21,48 @@ console.log(nombre, edad, esEstudiante);
     - Declara una variable numérica (**`edad`**) y otra variable numérica (**`edadEnDias`**) y asígnales valores. Luego, realiza una operación que convierta la edad a días y almacena el resultado en **`edadEnDias`**.
     - Declara una variable numérica (**`radio`**) y otra variable numérica (**`area`**) y asígnales valores. Luego, realiza una operación que calcule el área de un círculo con el radio dado y almacena el resultado en **`area`**. */
 
-    console.log("Ejercicio 2: \n");
-    let num1, num2;
-    num1 = 10;
-    num2 = 5;
-    
-    console.log( 
-        "Suma: " + (num1 + num2),
-        "\nResta: " + (num1 - num2),
-        "\nMultiplicacion: " + (num1 * num2),
-        "\nDivisión: " + (num1 / num2)
-    );
-    
-    edad = 4;   //Variable declarada en Ejercicio 1
-    const daysInYears =  365.25;
-    let edadEnDias = edad * daysInYears;
-    console.log (edadEnDias);
-    
-    const PI = 3.14159265359;
-    let radio = 3;
-    let area = PI * radio ** 2;
-    console.log( "\n Area: " + area );
-    
+console.log("Ejercicio 2: \n");
+let num1, num2;
+num1 = 10;
+num2 = 5;
+
+console.log( 
+    "Suma: " + (num1 + num2),
+    "\nResta: " + (num1 - num2),
+    "\nMultiplicacion: " + (num1 * num2),
+    "\nDivisión: " + (num1 / num2)
+);
+
+edad = 4;   //Variable declarada en Ejercicio 1
+const daysInYears =  365.25;
+let edadEnDias = edad * daysInYears;
+console.log("Edad en Dias: " + edadEnDias);
+
+const PI = 3.14159265359;
+let radio = 3;
+let area = PI * radio ** 2;
+console.log( "Area: " + area );
+
+//Ejercicio 3:
+
+/* 3. **Operaciones de comparación**
+    - Declara dos variables numéricas (**`num1`** y **`num2`**) y asígnales valores. Luego, utiliza los operadores de comparación para determinar si **`num1`** es mayor que **`num2`**, si **`num1`** es menor o igual a **`num2`**, y si **`num1`** es igual a **`num2`**.
+    - Declara una variable numérica (**`edad`**) y otra variable numérica (**`edadMaxima`**) y asígnales valores. Luego, utiliza los operadores de comparación para determinar si **`edad`** es mayor que **`edadMaxima`** y almacena el resultado en una variable booleana llamada **`esMayor`**. */
+
+console.log("Ejercicio 3: \n");
+
+num1 = prompt("Ejercicio 3:\nIngresar Numero 1: ");
+num2 = prompt("Ejercicio 3:\nIngresar Numero 2: ");
+
+console.log( 
+    "Num1 es Mayor a num2?: " + (num1>num2),
+    "\nNum1 es Menor o igual a num2?: " + (num1<=num2),
+    "\nNum1 es igual a num2?: " + (num1 === num2)
+);
+
+edad = 17;
+const edadMaxima = 18;
+let esMayor = edad >= edadMaxima;
+console.log("Edad: " + edad);
+console.log("Es Mayor de Edad?: " + esMayor);
+
